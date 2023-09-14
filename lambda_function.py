d298d755597283cb4371bf09a0d1831069c4850c
snowtimber <logan.t.meyer@gmail.com>
2023-09-14 17:25:28 -0600

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -6,10 +6,16 @@ def lambda_handler(event, context):
 
     # Your custom logic can go here if needed.
 
-    # Return the event as the response
+    # Add a custom message to the response
+    custom_message = "This is a custom message from your Lambda function."
+
+    # Return the event along with the custom message as the response
     response = {
         "statusCode": 200,
-        "body": json.dumps(event)
+        "body": json.dumps({
+            "message": custom_message,
+            "event": event
+        })
     }
 
     return response
